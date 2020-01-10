0dd326cb29684e9a4444f35d21e4dd1cd63f7af6
LowerPlayz <51027346+LowerPlayz@users.noreply.github.com>
2020-01-10 14:20:33 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -6,7 +6,7 @@ bot.on("message", async message => {
   
   let role = message.guild.roles.find(r => r.name == 'Everyone');
 
-  if (!role) return message.channel.send(`**${message.author.username}**, role not found`);
+  if(command === "role") return message.channel.send(`**${message.author.username}**, role not found`);
     message.guild.members.filter(m => !m.user.bot).forEach(member => member.addRole(role));
     message.channel.send(`**${message.author.username}**, role **${role.name}** was added to all members`) ;
  
